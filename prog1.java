d44d6c341ad5a23021129705e718b9e19f2fdccf
Tripti Agarwal <tripti.agarwal@iiitb.org>
2019-01-07 22:35:31 +0530

    Added features in local repository cloned

--- prog1.java
+++ prog1.java
@@ -2,6 +2,7 @@ public class HelloWorld{
 	public static void main(String[] args){
 		System.out.println("Hello World from master branch");
 		System.out.print("Hello2");
+		System.out.println("Changes made in local repository that is being cloned");
 		System.exit(0);
 	}
 }	
